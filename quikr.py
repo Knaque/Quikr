fb866dc5e52290d40772aeb2b97d5ec4f76d804b
William Moore <williammoore6105@gmail.com>
2019-10-17 15:01:23 -0500

    Added quikr.py

---
+++ quikr.py
@@ -0,0 +1,13 @@
+"""Quikr - Development made quikr."""
+
+import os
+
+quikr_start_message = True
+
+
+def clr():
+    """Clear terminal."""
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+# continue
