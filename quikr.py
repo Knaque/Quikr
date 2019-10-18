505705ee9556f4a02f1c54f2a92266ebf822a063
William Moore <williammoore6105@gmail.com>
2019-10-18 11:55:32 -0500

    Added python_version var, improved repeat func

--- quikr.py
+++ quikr.py
@@ -1,6 +1,14 @@
-"""Quikr - Development, made quikr."""
+"""Quikr - Development made quikr."""
 
 import os
+import platform
+
+
+class InvalidInputError(Exception):
+    """Throw error if function input is invalid."""
+
+
+python_version = str(platform.python_version())
 
 
 def clr():
@@ -8,7 +16,14 @@ def clr():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 
-def repeat(text):
-    """Infinitely repeats specified text."""
-    while True:
-        print(text)
+def repeat(text, times="inf"):
+    """Repeats specified text a specified amount."""
+    if times == "inf":
+        while True:
+            print(text)
+    elif isinstance(times, int) is True:
+        for x in range(0, times):
+            print(text)
+    else:
+        raise InvalidInputError("Second argument "
+                                "must be 'inf' or an integer.")
