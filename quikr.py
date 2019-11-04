1c9643f26676db32b1e50c2cf5dff2036810246c
Reilly Moore <williammoore6105@gmail.com>
2019-11-04 10:23:01 -0600

    Update quikr

--- quikr.py
+++ quikr.py
@@ -12,11 +12,15 @@ class InvalidInputError(Exception):
 
 
 # ============================== Variables ===============================
-# Variable for calling current Python version
-# NOTE: Can someone work out a way of doing this so that platform is only
-# imported if the variable is called? Using this in a function and calling as a
-# var adds a "None" line for some reason. ~Reilly
+# NOTE: Can someone work out a way of doing these so that platform is only
+# imported if the variables are called? Using this in a function and calling
+# as a variable [ex. print(q.py_version())] adds a "None" line for some reason.
+
+# Variable for current Python version
 py_version = str(platform.python_version())
+
+# Variable for current OS
+os = str(platform.system())
 # ========================================================================
 
 
