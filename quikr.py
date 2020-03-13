b0507db7c1901b7a9db36835a7878b77d828da7b
Delight Ibrahim <fatmotherhugger@protonmail.com>
2020-03-12 23:11:45 -0500

    Fixed rainbow(), tested to work versions 2.7+

--- quikr.py
+++ quikr.py
@@ -44,34 +44,23 @@ def repeat(text, times="inf"):
                                    "be 'inf' or an integer.")
 
 
-"""
 def rainbow(string):
-    Converts text into a colored rainbow of said text, currently only works in version 2.7.
+    '''Converts text (str) into a colored rainbow of said text, tested to work on versions 2.7+.'''
     # TODO: TheCrappyCoder, Ensure that this user input is sanitized and make sure that you add a try except statment in case string is not str
-    # TODO: TheCrappyCode, make a version of this that works for python versions == or >= 2.7
-    if "2.7" in platform.python_version():
-        from colorama import Fore
-        from itertools import cycle
-        nums = [1, 2, 3, 4, 5]  # Since the colorama module doesn't have a few colors commonly found on the rainbow, we are limited to five if you count my Magenta substitute
-        colorful = ""
-        for char, x in zip(string, cycle(nums)):
-            # TODO: TheCrappyCoder, Find a more efficent method for this you buffon, and make this run properly
-            if x == 1:
-                colorful += Fore.RED + char
-            elif x == 2:
-                colorful += Fore.YELLOW + char
-            elif x == 3:
-                colorful += Fore.GREEN + char
-            elif x == 4:
-                colorful += Fore.BLUE + char
-            elif x == 5:
-                colorful += Fore.MAGENTA + char
-        return colorful
-    elif "2.7" not in platform.python_version():
-        print("This function only works in Python 2.7, we'll get around to adding a working version for more versions soon.")
-        exit()
-
-
-print(rainbow("Rainbow"))
-"""
+    from itertools import cycle
+    nums = [1, 2, 3, 4, 5]
+    colorful = ""
+    for char, x in zip(string, cycle(nums)):
+        # TODO: TheCrappyCoder, Find a more efficent method for this, you buffon
+        if x == 1:
+            colorful += "\033[0;31m" + char  # Red
+        elif x == 2:
+            colorful +=  "\033[1;33m" + char  # Yellow (we can't go in sequential order since the ANSI standard doesn't have an orange)
+        elif x == 3:
+            colorful += "\033[0;32m" + char  # Green
+        elif x == 4:
+            colorful += "\033[0;34m" + char  # Blue
+        elif x == 5:
+            colorful += "\033[0;35m" + char  # Purple (No ANSI Indigo or Violet)
+    return colorful
 # ========================================================================
